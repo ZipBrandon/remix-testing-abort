f364f0c685ad222ebefab8828e4d2b46eab29dbb
Brandon Lorenz <blorenz@gmail.com>
2023-10-16 09:40:58 -0400

    first commits

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,29 +17,7 @@ export default function Index() {
 
           <Link to={'/testing'}>Testing</Link>
         </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+
       </ul>
     </div>
   );
